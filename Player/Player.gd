8ef8de50cb51b7b6bf063b51a970d87268749d84
winter-pitts <wmpitts@iu.edu>
2022-09-18 00:03:00 -0400

    Complete Exercise 2c

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,11 @@ var velocity = Vector2.ZERO
 var rotation_speed = 5.0
 var speed = 5.0
 var max_speed = 400.0
+var health = 10
+
+
+var Effects = null
+onready var Explosion = load("res://Effects/Explosion.tscn")
 
 
 onready var Bullet = load("res://Player/Bullet.tscn")
@@ -42,4 +47,21 @@ func get_input():
 		rotation_degrees = rotation_degrees + rotation_speed
 	return to_return.rotated(rotation)
 	
+	
+func damage(d):
+	health -= d
+	if health <= 0:
+		Effects = get_node_or_null("root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instance()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+			hide()
+			yield(explosion, "animation_finished")
+		queue_free()
 		
+
+
+func _on_Area2D_body_entered(body):
+	if body.name != "Player":
+		damage(100)
