38de6d339d13bc7888e223024574495ae8d3b1cf
winter-pitts <wmpitts@iu.edu>
2022-09-18 00:07:46 -0400

    Complete Exercise - Forgot to Save

--- Player/Player_Container.gd
+++ Player/Player_Container.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+onready var Player = load("res://Player/Player.tscn")
+
+func _physics_process(_delta):
+	if get_child_count() == 0:
+		var player = Player.instance()
+		player.position = Vector2(512,300)
+		add_child(player)
 
 func _ready():
 	pass
